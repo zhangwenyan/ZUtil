abcf391b1545c88661f39e131839e1ac9914bc2b
zwyhome <799378666@qq.com>
2017-02-27 10:05:22 +0800

    ...

--- ZUtil/ConfigUtil.cs
+++ ZUtil/ConfigUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ZUtil
@@ -26,6 +27,19 @@ namespace ZUtil
         public static String readSetting(String key)
         {
             return readSetting(key, null);
+        }
+
+        public  static void changeSetting(String key, String value)
+        {
+            //调用  
+            string assemblyConfigFile = Assembly.GetEntryAssembly().Location;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(assemblyConfigFile);    //获取appSettings节点 
+            AppSettingsSection appSettings = (AppSettingsSection)config.GetSection("appSettings");
+            //删除name，然后添加新值  
+            appSettings.Settings.Remove(key);
+            appSettings.Settings.Add(key, value);
+            //保存配置文件  
+            config.Save();
         }
 
     }
