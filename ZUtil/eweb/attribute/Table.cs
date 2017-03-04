0b6c69ca77838b13fc3f7b75b84fe7f300971200
799378666@qq.com <799378666@qq.com>
2017-03-04 18:38:32 +0800

    '...'

---
+++ ZUtil/eweb/attribute/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZUtil.eweb.attribute
+{
+    public class Table:Attribute
+    {
+        public String tbName { get; set; }
+    }
+}
